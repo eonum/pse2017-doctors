b81817793937194091d6c91c96e881db26010b91
Tim Peter <tim.peter@eonum.ch>
2015-12-09 13:56:02 +0100

    Basic HAML version of comparison view.

--- app/helpers/comparisons_helper.rb
+++ app/helpers/comparisons_helper.rb
@@ -1,2 +1,10 @@
 module ComparisonsHelper
+  def format_hospital_variable variable, hospital, comparison
+    value = hospital[variable.field_name]
+    return '' if value.nil?
+    value = value[comparison.base_year] if variable.is_time_series
+    return '' if value.nil?
+
+    value
+  end
 end
