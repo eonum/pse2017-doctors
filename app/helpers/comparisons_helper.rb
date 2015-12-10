ffebded040263779af00b5918f7d0cb633114121
Tim Peter <tim.peter@eonum.ch>
2015-12-10 11:10:48 +0100

    Styling flags.

--- app/helpers/comparisons_helper.rb
+++ app/helpers/comparisons_helper.rb
@@ -5,6 +5,7 @@ module ComparisonsHelper
     value = value[comparison.base_year] if variable.is_time_series
     return '' if value.nil?
     return "#{'%.1f' % value}%" if variable.variable_type == :percentage
+    return !value.blank? && value.upcase == 'X' ? image_tag('accept.png', :style => 'border-style:none') : '' if variable.variable_type == :boolean
     value
   end
 
@@ -15,6 +16,7 @@ module ComparisonsHelper
     value = value[comparison.base_year] if variable.is_time_series
     limit = variable.highlight_threshold
     classes << 'orange-highlight' if(limit > 0 && limit < 100 && limit <= value)
+    classes << 'text-center' if variable.variable_type == :boolean
 
     return classes.join(' ')
   end
