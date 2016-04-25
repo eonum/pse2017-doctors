c85139c3d0a329aa49c1f93c18f6cfa0623d1842
lukovski <lukas.bosshart@students.unibe.ch>
2016-04-25 10:27:16 +0200

    Fixed some Synatx-Errors in the tests.

--- test/controllers/admin/admin_controller_test.rb
+++ test/controllers/admin/admin_controller_test.rb
@@ -6,23 +6,23 @@ class Admin::AdminController < ApplicationController::TestCase
   end
 
   test 'should redirect if doctor' do
-    get (:admin_only, {'current_user.is_admin' => false},{'current_user.email' => "asdf@qualitaetsmedizin.ch"})
+    get :admin_only, {'current_user.is_admin' => false},{'current_user.email' => "asdf@qualitaetsmedizin.ch"}
     assert(redirect?)
   end
 
   test "should get doctor path" do
-    get (:get_doctor_path, {'current_user.is_admin' => false},{'current_user.email' => "asdf@qualitaetsmedizin.ch"})
+    get :get_doctor_path, {'current_user.is_admin' => false},{'current_user.email' => "asdf@qualitaetsmedizin.ch"}
     assert_equal"/de/admin/doctors/asdf", @response
   end
 
   test "should select admin layout" do
-    get (:select_layout, {'current_user.is_admin' => true})
+    get :select_layout, {'current_user.is_admin' => true}
 
     assert_equal"admin", @response
   end
 
   test "should select doctor_user layout" do
-    get (:select_layout, {'current_user.is_admin' => false})
+    get :select_layout, {'current_user.is_admin' => false}
 
     assert_equal"doctor_user", @response
   end
